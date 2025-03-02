4c5586cebd7e3a7cb4b77727b1f6875ac170c256
tejas-pagare <147067782+tejas-pagare@users.noreply.github.com>
2025-03-02 08:03:19 +0530

    Update index.js

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.json());
 app.use(cors({
-  origin: 'http://localhost:5173', // Allow requests from frontend
+  origin: '*' // Allow requests from frontend
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
 }));
